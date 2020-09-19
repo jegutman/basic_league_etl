fdf79f9c4b2311a82118bc7b2452cb0244fa29be
JOSHUA GUTMAN <jegutman@gmail.com>
2020-09-19 13:03:27 -0700

    make db a parameter

--- sql_tools.py
+++ sql_tools.py
@@ -6,27 +6,27 @@ connection = MySQLdb.connect(host='localhost', user=db_user, passwd=db_passwd, c
 cursor = connection.cursor()
 cursor.execute("SET NAMES utf8")
 
-def get_matches(region=None):
+def get_matches(region=None, db="soloq"):
     if region is not None:
-        sql = "SELECT distinct match_id FROM soloq.games WHERE region = '%(region)s'"
+        sql = "SELECT distinct match_id FROM %(db)s.games WHERE region = '%(region)s'"
         cursor.execute(sql % locals())
         connection.commit()
         return set([_i for (_i,) in cursor.fetchall()])
     else:
-        sql = "SELECT distinct match_id FROM soloq.games"
+        sql = "SELECT distinct match_id FROM %(db)s.games"
         cursor.execute(sql % locals())
         connection.commit()
         return set([_i for (_i,) in cursor.fetchall()])
 
-def get_last_game(player):
-    sql = """SELECT creation FROM soloq.lane_info join soloq.games using(match_id) WHERE player_name = '%(player)s' order by creation desc limit 3"""
+def get_last_game(player, db="soloq"):
+    sql = """SELECT creation FROM %(db)s.lane_info join %(db)s.games using(match_id) WHERE player_name = '%(player)s' order by creation desc limit 3"""
     cursor.execute(sql % locals())
     res = [x for (x,) in cursor.fetchall()]
     if len(res) == 0: return 0
     return res[-1]
 
-def load_game(match_id, version, result, creation, duration, region):
-    sql = """INSERT INTO soloq.games (match_id, version, result, creation, duration, region)
+def load_game(match_id, version, result, creation, duration, region, db="soloq"):
+    sql = """INSERT INTO %(db)s.games (match_id, version, result, creation, duration, region)
                  VALUES (%(match_id)s, '%(version)s', %(result)s, %(creation)s, %(duration)s, '%(region)s')
                  ON DUPLICATE KEY UPDATE
                  match_id = %(match_id)s;
@@ -35,8 +35,8 @@ def load_game(match_id, version, result, creation, duration, region):
     cursor.execute(sql % locals())
     connection.commit()
 
-def load_bans(match_id, region, is_blue, ban1, ban2, ban3, ban4, ban5):
-    sql = """INSERT INTO soloq.bans (match_id, region, is_blue, ban1, ban2, ban3, ban4, ban5)
+def load_bans(match_id, region, is_blue, ban1, ban2, ban3, ban4, ban5, db="soloq"):
+    sql = """INSERT INTO %(db)s.bans (match_id, region, is_blue, ban1, ban2, ban3, ban4, ban5)
                  VALUES (%(match_id)s, '%(region)s', %(is_blue)s, "%(ban1)s", "%(ban2)s", "%(ban3)s", "%(ban4)s", "%(ban5)s")
                  ON DUPLICATE KEY UPDATE
                  match_id = %(match_id)s;
@@ -45,8 +45,8 @@ def load_bans(match_id, region, is_blue, ban1, ban2, ban3, ban4, ban5):
     cursor.execute(sql % locals())
     connection.commit()
 
-def load_lane_info(match_id, region, role, is_blue, player_name, champ, result):
-    sql = """INSERT INTO soloq.lane_info (match_id, region, role, is_blue, player_name, champ, result)
+def load_lane_info(match_id, region, role, is_blue, player_name, champ, result, db="soloq"):
+    sql = """INSERT INTO %(db)s.lane_info (match_id, region, role, is_blue, player_name, champ, result)
                  VALUES (%(match_id)s, '%(region)s', '%(role)s', %(is_blue)s, '%(player_name)s', "%(champ)s", %(result)s)
                  ON DUPLICATE KEY UPDATE
                  match_id = %(match_id)s;
@@ -55,8 +55,8 @@ def load_lane_info(match_id, region, role, is_blue, player_name, champ, result):
     cursor.execute(sql % locals())
     connection.commit()
 
-def load_stats(match_id, region, role, is_blue, champ, result, kills, deaths, assists, cc_score, gold_spent, damage, heal, tank, cs10, cs20, g10, g20, xp10, xp20):
-    sql = """INSERT INTO soloq.stats (
+def load_stats(match_id, region, role, is_blue, champ, result, kills, deaths, assists, cc_score, gold_spent, damage, heal, tank, cs10, cs20, g10, g20, xp10, xp20, db="soloq"):
+    sql = """INSERT INTO %(db)s.stats (
                 match_id,
                 region,
                 role,
